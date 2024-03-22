5ca0770429ffba974214f77804cd96cdc8a0091f
houdini_077 <mohameddenna077@gmail.com>
2024-03-22 03:13:22 +0100

    full code

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const naam = document.getElementById("name");
-const gender = document.getElementById("gender");
-
 function getGender() {
+    const naam = document.getElementById("name-gen");
+    const gender = document.getElementById("gender");
     const url = `https://api.genderize.io?name=${naam.value}`;
+    
     fetch(url)
     .then(res => res.json())
     .then(data => {
@@ -14,3 +14,48 @@ function getGender() {
     })
     .catch(error => console.error('Error fetching gender:', error));
 };
+
+function dataUsa() {
+    const url = 'https://datausa.io/api/data?drilldowns=Nation&measures=Population';
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+
+        const tableBody = document.querySelector('.container-2 tbody');
+        tableBody.innerHTML = '';
+
+        data.data.forEach(entry => {
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td>${entry['Nation']}</td>
+                <td>${entry['Population']}</td>
+                <td>${entry['Year']}</td>
+            `;
+            tableBody.appendChild(row);
+        });
+    })
+    .catch(error => console.error('Error fetching data:', error));
+}
+
+function nationalize() {
+    const name = document.getElementById("name-nat");
+    const nationality = document.getElementById("nationality");
+    const url = `https://api.nationalize.io?name=${name.value}`;
+    
+    fetch(url)
+    .then (res => res.json())
+    .then(data => {
+        if (data.country && data.country.length > 0) {
+
+            const mostLikelyCountry = data.country[0];
+            nationality.innerHTML = mostLikelyCountry.country_id;
+            
+        } else {
+            nationality.innerHTML = "Nationality not found";
+        }
+    })
+    .catch(error => console.error('Error fetching nationality:', error));
+}
+
+
+
